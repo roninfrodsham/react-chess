51eae05f0ee361fe6571723dc8a78d70b35c2bd1
roninfrodsham <roninfrodsham@gmail.com>
2024-01-02 16:47:42 +0000

    set board boundaries for pieces

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -29,15 +29,33 @@ const Chessboard = () => {
   };
 
   const movePiece = (e: React.MouseEvent<Element, MouseEvent>) => {
-    if (selectedPiece) {
+    const chessboard = chessboardRef.current;
+    if (selectedPiece && chessboard) {
       const elementInfo = selectedPiece.getBoundingClientRect();
-      selectedPiece.style.position = "absolute";
-      selectedPiece.style.top = `${
-        e.clientY - chessboardRef.current.offsetTop - elementInfo.height / 2
-      }px`;
-      selectedPiece.style.left = `${
-        e.clientX - chessboardRef.current.offsetLeft - elementInfo.width / 2
-      }px`;
+      const offsetX = chessboard.offsetLeft;
+      const offsetY = chessboard.offsetTop;
+      const minX = 0;
+      const maxX = chessboard.clientWidth - elementInfo.width;
+      const minY = 0;
+      const maxY = chessboard.clientHeight - elementInfo.height;
+      const mouseX = e.clientX - offsetX - elementInfo.width / 2;
+      const mouseY = e.clientY - offsetY - elementInfo.height / 2;
+
+      if (mouseX < minX) {
+        selectedPiece.style.left = `${minX}px`;
+      } else if (mouseX > maxX) {
+        selectedPiece.style.left = `${maxX}px`;
+      } else {
+        selectedPiece.style.left = `${mouseX}px`;
+      }
+
+      if (mouseY < minY) {
+        selectedPiece.style.top = `${minY}px`;
+      } else if (mouseY > maxY) {
+        selectedPiece.style.top = `${maxY}px`;
+      } else {
+        selectedPiece.style.top = `${mouseY}px`;
+      }
     }
   };
 
